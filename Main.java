5628a144648ed75b76ebc4fe1a90057454135ff9
b1ckw <victorcfsc@dcomp.ufs.br>
2018-09-23 06:14:47 -0300

    Alteração Simples no Print

--- Main.java
+++ Main.java
@@ -12,11 +12,9 @@ public class Main {
     int[] fluxo = ff.ffflow(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 
     System.out.print("[DONE]    Fluxo  :      ");
-    System.out.print("[");
     for (int i = 0; i < fluxo.length; i++) {
       System.out.print(fluxo[i] + " ");
     }
-    System.out.print("]");
     System.out.println();
 
   }
